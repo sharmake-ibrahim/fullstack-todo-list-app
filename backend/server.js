8c12dd41960b11b054ac9e268cb4a7fb1eda1e56
Sharmake Ibrahim <sharmakeahmedibrahim@gmail.com>
2025-01-16 20:01:59 +1300

    all routes and origin allowed

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,7 @@ app.use(express.json())
 
 const cors = require("cors");
 
-app.use(cors({
-    origin: "https://fullstack-todo-list-app.vercel.app", 
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true 
-}));
+app.use(cors());
 
 require("dotenv").config();
 
